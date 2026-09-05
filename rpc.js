45983e25e4cd32a2d152c7d07b18eb1b443e5979
lovawin <89751634+lovawin@users.noreply.github.com>
2026-09-05 13:51:40 -0400

    Fix import path for config.js in rpc.js

--- rpc.js
+++ rpc.js
@@ -1,5 +1,5 @@
 import { RPC_URL, FACTORY_ADDRESS, WETH_ADDRESS } from "./config.js";
-import { RPC_URL, FACTORY_ADDRESS, WETH_ADDRESS } from "../config.js";
+
 
 export const provider = new ethers.JsonRpcProvider(RPC_URL);
 
